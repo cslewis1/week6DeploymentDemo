2b515f0de4027e8721f27b79473a863a34d1172b
Charneaka Lewis <clewis20@ggc.edu>
2023-05-10 13:34:49 -0400

    correcting spelling

--- deployment-demo/public/main.js
+++ deployment-demo/public/main.js
@@ -2,7 +2,7 @@ const catBtn = document.querySelector('#cat')
 const dogBtn = document.querySelector('#dog')
 
 const catHandler = () => alert('The cat name is: Rascal')
-const dogHandeler = () => alert('The dog name is: Doug' )
+const dogHandler = () => alert('The dog name is: Doug' )
 
 catBtn.addEventListener('click', catHandler)
 dogBtn.addEventListener('click', dogHandler)
